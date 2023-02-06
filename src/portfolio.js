924e50d0472256afbd2c0341aa3d8d967aef4c7f
Dillon Cleaver <54515102+dilloncleaver@users.noreply.github.com>
2023-02-06 11:08:27 -0600

    Update portfolio.js
    Updated GitHub profile to link to my Secret Lab GH account for now.

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const about = {
   // resume: 'https://example.com',
   social: {
     linkedin: 'https://www.linkedin.com/in/dillon-cleaver/',
-    github: 'https://github.com/cleadi',
+    github: 'https://github.com/dillon-secretlab',
   },
 }
 
